6cfe137c91b0c193e18152ac838ca12f62d23399
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-11-24 10:33:37 +0530

    fix : config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,11 @@
 import { defineCollection, z } from "astro:content";
 
 export const collections = {
-  projects: defineCollection({
+  work: defineCollection({
     schema: z.object({
       title: z.string(),
       description: z.string(),
-      publishDate: z.coerce.date(),
+      priority: z.coerce.number(),
       tags: z.array(z.string()),
       img: z.string(),
       img_alt: z.string().optional(),
