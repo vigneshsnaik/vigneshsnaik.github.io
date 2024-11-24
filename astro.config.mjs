bb0be4dfd604553a5060ebb7f50801a5d7b8a649
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-11-24 11:45:53 +0530

    add : robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vigneshsnaik.github.io",
-  integrations: [sitemap()],
+  integrations: [sitemap(), robotsTxt()],
   output: "static",
 });
